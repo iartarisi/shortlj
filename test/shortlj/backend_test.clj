406ed6e1718a76c834494e56b91909ee67abc119
Ionuț Arțăriși <ionut@artarisi.eu>
2012-06-29 21:35:13 +0200

    add licensing

--- test/shortlj/backend_test.clj
+++ test/shortlj/backend_test.clj
@@ -1,3 +1,20 @@
+;; -*- coding: utf-8 -*-
+;; Copyright (c) 2012 Ionuț Arțăriși <ionut@artarisi.eu>
+;; This file is part of Shortlj.
+
+;; Shortlj is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Shortlj is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Shortlj.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns shortlj.backend_test
   (:use clojure.test)
   (:use shortlj.backend))
