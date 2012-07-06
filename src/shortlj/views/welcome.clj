1d9fe93cd6357308e72ae90be90caf4b26a8225f
Ionuț Arțăriși <ionut@artarisi.eu>
2012-07-06 17:21:48 +0200

    delete unused file

--- src/shortlj/views/welcome.clj
+++
@@ -1,8 +0,0 @@
-(ns shortlj.views.welcome
-  (:require [shortlj.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to shortlj"]))
